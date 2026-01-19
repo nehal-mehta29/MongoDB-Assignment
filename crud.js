56c5d044d4b07aaa98f0f3ec674ae98d11788c34
Nehal Mehta <nehal29096@gmail.com>
2026-01-19 18:17:19 +0530

    update

--- crud.js
+++ crud.js
@@ -17,3 +17,30 @@ db.tickets.find()
 
 // View all categories
 db.categories.find()
+
+
+/* ---------- UPDATE OPERATIONS ---------- */
+
+//Updating role 
+db.users.updateOne(
+    { userId: "user20" },
+    { $set: { role: "organizer" } }
+)
+
+//Updating event status
+db.events.updateOne(
+    { eventId: "event18" },
+    { $set: { status: "upcoming" } }
+)
+
+//Updating ticket status
+db.tickets.updateOne(
+    { ticketId: "ticket7" },
+    { $set: { status: "cancelled" } }
+)
+
+//Updating categories description
+db.categories.updateOne(
+    { categoryId: "cat18" },
+    { $set: { description: "Cultural college events" } }
+)
