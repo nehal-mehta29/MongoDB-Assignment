b8f5c636a071173afff372cfd6b8ee09574b9b37
Nehal Mehta <nehal29096@gmail.com>
2026-01-19 18:09:55 +0530

    read

--- crud.js
+++ crud.js
@@ -2,3 +2,18 @@
 
 /* ---------- CREATE ---------- */
 //Already done refer users.js, events.js, tickets.js, categories.js 
+
+
+/* ---------- READ OPERATIONS ---------- */
+
+// View all users
+db.users.find()
+
+// View all events
+db.events.find()
+
+// View all tickets
+db.tickets.find()
+
+// View all categories
+db.categories.find()
