692d7416f18064d43eceec522b082629b3649d75
Nehal Mehta <nehal29096@gmail.com>
2026-01-19 16:23:09 +0530

    schema for users added

---
+++ schema.js
@@ -0,0 +1,11 @@
+/* =============================== USERS COLLECTION SCHEMA =============================== */
+
+const UsersSchema = {
+  _id: "ObjectId",
+  userId: "String",
+  name: "String",
+  email: "String",
+  phone: "Number",
+  role: "String",
+  createdAt: "Date"
+};
