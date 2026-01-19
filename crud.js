aa2dbfb01c7b7627ed679c39f38b2ac4652d2616
Nehal Mehta <nehal29096@gmail.com>
2026-01-19 17:52:54 +0530

    create

---
+++ crud.js
@@ -0,0 +1,4 @@
+/* ========================================= CRUD OPERATIONS ========================================= */
+
+/* ---------- CREATE ---------- */
+//Already done refer users.js, events.js, tickets.js, categories.js 
