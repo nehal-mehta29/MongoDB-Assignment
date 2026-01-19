1cd95ee296b78101d6c6d4c11fba00f6d5718c96
Nehal Mehta <nehal29096@gmail.com>
2026-01-19 16:48:51 +0530

    initial

---
+++ categories.js
@@ -0,0 +1,4 @@
+/* =============================== CATEGORIES COLLECTION =============================== */
+
+//This file is to create and add sample category documents in the categories collection 
+
