ee9be1a965fe0c7cfbaff2520b251a6766c8ce61
Nehal Mehta <nehal29096@gmail.com>
2026-01-19 16:33:14 +0530

    events schema added

--- schema.js
+++ schema.js
@@ -1,12 +1,28 @@
 /* =============================== USERS COLLECTION SCHEMA =============================== */
 
 const UsersSchema = {
-  _id: "ObjectId",
-  userId: "String",
-  name: "String",
-  email: "String",
-  phone: "Number",
-  role: "String",
-  createdAt: "Date"
-};
+    _id: "ObjectId",
+    userId: "String",
+    name: "String",
+    email: "String",
+    phone: "Number",
+    role: "String",
+    createdAt: "Date"
+}
 
+/* =============================== EVENTS COLLECTION SCHEMA =============================== */ 
+
+const EventsSchema = {
+    _id: "ObjectId",
+    eventId: "String",
+    title: "String",
+    description: "String",
+    category: "String",
+    dateTime: "String",
+    venue: "String",
+    organizerId: "String",
+    price: "Number",
+    totalTickets: "Number",
+    availableTickets: "Number",
+    status: "String"
+}
